cf29c0b6df97c4c66c23a4c1a2fc7565ee6ff46a
chadyred <chadyred@gmail.com>
2018-01-16 10:43:23 +0100

    Add...

--- models/article.js
+++ models/article.js
@@ -2,8 +2,19 @@
 module.exports = (sequelize, DataTypes) => {
   var Article = sequelize.define('Article', {
     title: DataTypes.STRING,
-    text: DataTypes.TEXT
+    text: DataTypes.TEXT,
+    createdAt: {
+      type: DataTypes.DATE(3),
+      field: 'createdAt',
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)'),
+    },
+    updatedAt: {
+      type: DataTypes.DATE(3),
+      field: 'updatedAt',
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)'),
+    }
   }, {
+    timestamps: true,
     classMethods: {
       associate: function(models) {
         
